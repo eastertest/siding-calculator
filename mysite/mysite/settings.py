8d6f15f375b9cfb97143734e270d28e43d79dc30
Jess Easter <jeaster2@my.waketech.edu>
2021-05-07 19:42:13 -0400

    Dockerfile

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -79,6 +78,7 @@ DATABASES = {
         'NAME': 'sampledb',
         'USER': 'userVJ2',
         'PASSWORD': 'DVCBCGnTxi6XM8VX',
+        #'HOST': '127.0.0.1',
         'HOST': '172.17.0.11',
         'PORT': '5432'
     }
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
